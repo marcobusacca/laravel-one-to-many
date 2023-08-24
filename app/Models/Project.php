2520b476037df2566335983a44d4913e13dcdd54
marcobusacca <marcobusacca01@gmail.com>
2023-08-24 15:05:30 +0200

    [FEAT] Project and Type Models function relations

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Type;
+
 class Project extends Model
 {
     use HasFactory;
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 
     protected $fillable = ['title', 'description', 'date_of_creation', 'cover_image'];
 }
